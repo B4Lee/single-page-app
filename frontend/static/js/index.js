fc439b562909cf3d234a16a32f9ebb616f5d9243
B4Le <iqbalanggoro48@gmail.com>
2022-12-13 21:28:19 +0700

    Update index.js

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -2,32 +2,42 @@ import Dashboard from "./views/Dashboard.js";
 import Posts from "./views/Posts.js";
 import Settings from "./views/Settings.js";
 
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$" );
+
+const getParams = match => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    return {}
+}
 
 const navigateTo = url => {
     history.pushState(null, null, url);
     router();
 }
 
-
 /**
  * It takes a list of routes and a location and returns the route that matches the location.
  */
 const router = async () => {
+    console.log(pathToRegex("/posts/:id"));
+    // /posts/:id
     const routes = [
-        { path: "/", view: Dashboard},
-        { path: "/posts", view: Posts},
-        { path: "/settings", view: Settings},
+        { path: "/", view: Dashboard },
+        { path: "/posts", view: Posts },
+        { path: "/posts/:id", view: Posts },
+        { path: "/settings", view: Settings },
     ];
 
 
     const potentialMatches = routes.map(route => {
         return {
             route: route,
-            isMatch: location.pathname === route.path
+            result: location.pathname.match(pathToRegex(route.path))
         };
     });
 
-    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
 
     if (!match) {
         match = {
@@ -36,7 +46,7 @@ const router = async () => {
         }
     }
 
-    const view = new match.route.view();
+    const view = new match.route.view(getParams(match));
 
     document.querySelector("#app").innerHTML = await view.getHtml();
 
